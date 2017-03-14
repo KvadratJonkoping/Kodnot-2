72439aab53563414a637fe1e88b8484c5abd6623
jonathanmourtada <jonathan.mourtada@divid.se>
2017-03-14 07:31:10 +0100

    summarize time

--- jontem/typescript/src/index.ts
+++ jontem/typescript/src/index.ts
@@ -3,15 +3,21 @@ import { createTimeTeams, filterValidTimeTeams } from "./time-teams";
 
 function groupByClub(participants: Array<Participant>) {
     return participants.reduce((soFar, current) => {
-        if (soFar[current.team]) {
-            return {
-                ...soFar,
-                [current.team]: soFar[current.team].concat(current)
-            }
-        }
-
-        return { ...soFar, [current.team]: [current] };
+        return soFar[current.team]
+            ? { ...soFar, [current.team]: soFar[current.team].concat(current) }
+            : { ...soFar, [current.team]: [current] };
     }, {});
+}
+
+function sum(teamMembers: Array<Participant>) {
+    return teamMembers.reduce((soFar, current) => (soFar + current.time), 0);
+}
+
+function formatTime(numerical: number): string {
+    const minutes = Math.floor(numerical / 60);
+    const remaining = parseInt(Math.round(numerical % 60 * 100).toString()) / 100; // Round remaining part with 2 decimals. The JavaScript way :)
+
+    return `${minutes}min ${remaining}sec`;
 }
 
 const participants: Array<Participant> = parseParticipants("../../resultat.csv")
@@ -19,7 +25,7 @@ const participants: Array<Participant> = parseParticipants("../../resultat.csv")
 
 const participantByClub = groupByClub(participants);
 
-const result = Object.keys(participantByClub).reduce((soFar, currentClub) => {
+const result: { [teamId: string]: Array<Participant> } = Object.keys(participantByClub).reduce((soFar, currentClub) => {
     const timeTeams = createTimeTeams(participantByClub[currentClub]);
     const validTeams = filterValidTimeTeams(timeTeams);
     return Object.keys(validTeams)
@@ -35,5 +41,6 @@ for (const clubKey of Object.keys(result)) {
     const club = result[clubKey];
     console.log(`-------- ${clubKey} ---------`);
     console.log(club);
+    console.log(`clubtime: ${formatTime(sum(club))}`);
     console.log(`-------- end ---------`);
 }
