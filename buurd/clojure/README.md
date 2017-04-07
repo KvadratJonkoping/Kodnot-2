4838948bbfd59ce1bd18d31b2568f640add846fc
buurd <roland@javalia.se>
2017-04-07 22:57:01 +0200

    Första version som fungerar

--- buurd/clojure/README.md
+++ buurd/clojure/README.md
@@ -3,4 +3,15 @@ Försök att lösa kodnöt 2
 Installera java 8
 Installera leiningen
 
+Kör:
+lein repl
+
+Skriv:
+(-main)
+
+Förväntat output:
+Klubb 6 Wiliam Karlsson Oskar Oskarsson Jonna Nilsson 00:02:49.220
+Klubb 1 Lovisa Svensson Fredrik Andersson Lisa Svensson 00:02:59.350
+Klubb 6 John Svensson Thelma Karlsson Maja Karlsson 00:03:05.890
+
 
--- buurd/clojure/not2/src/not2/core.clj
+++ buurd/clojure/not2/src/not2/core.clj
@@ -1,8 +1,10 @@
 (ns not2.core
   (:gen-class)
-  (:require [clojure.string :as str]))
-
-
+  (:require   [clojure.string :as str]
+              [clojure.walk :as walk]
+              [clj-time.core :as t]
+              [clj-time.format :as f]
+              [clj-time.coerce :as c]))
 
 (defn read-file [path]
   "Läs upp innehållet i filen"
@@ -40,8 +42,7 @@
                   (into {}
                         (combine-keys-and-values
                           (get data :keys) x)))
-                (rest
-                  (get data :values)))))
+                  (get data :values))))
 
 (defn key-value-åkare [coll data]
   (assoc data :keyvalue-åkare (into {} (map (fn [åkare] (hash-map (get åkare :bib) åkare)) (get data :mapped-values)))))
@@ -67,7 +68,7 @@
                                          (get data :sorted)))))
 
 (defn remove-sequences [data]
-  (assoc data :no-sequences (clojure.walk/postwalk #(if (sequential? %) (vec %) %) (get data :keyvalue-klubb))))
+  (assoc data :no-sequences (walk/postwalk #(if (sequential? %) (vec %) %) (get data :keyvalue-klubb))))
 
 
 (defn find-next-without-team [klubb, num]
@@ -91,68 +92,142 @@
 
 (defn add-first-member
   ([klubb team]
-   (assoc (second (nth klubb (find-next-without-team klubb 0))) :team team))
+   (if (= -1 (find-next-without-team klubb 0))
+     klubb
+     (assoc (second (nth klubb (find-next-without-team klubb 0))) :team team)))
   ([klubb team kön]
-   (assoc (second (nth klubb (find-next-without-team-by-kön klubb 0 kön))) :team team)))
+   (if (= -1 (find-next-without-team-by-kön klubb 0 kön))
+     klubb
+     (assoc (second (nth klubb (find-next-without-team-by-kön klubb 0 kön))) :team team))))
 
 
-(defn update-team [data klubb åkare num]
-  (update-in data [:no-sequences klubb num 1] assoc :team "A"))
+(defn update-team [klubb åkare num team]
+  (if (= -1 num)
+    klubb
+    (update-in klubb [num 1] assoc :team team)))
 
 (defn add-member-to-team
-  ([data klubb team]
-   (update-team data klubb
+  ([klubb team]
+   (update-team klubb
                 (add-first-member
-                  (get
-                    (get data :no-sequences) klubb)
+                  klubb
                   team)
-                (find-next-without-team (get (get data :no-sequences) klubb) 0)))
-  ([data klubb team kön]
-   (update-team data klubb
+                (find-next-without-team klubb 0)
+                  team))
+  ([klubb team kön]
+   (update-team klubb
                 (add-first-member
-                  (get (get data :no-sequences) klubb)
+                  klubb
                   team)
-                (find-next-without-team-by-kön (get (get data :no-sequences) klubb) 0 kön))))
+                (find-next-without-team-by-kön klubb 0 kön)
+                team)))
 
-(defn add-first-two-members-to-team [data klubb team]
-  (add-member-to-team (add-member-to-team data klubb team) klubb team))
+(defn add-first-two-members-to-team [klubb team]
+  (add-member-to-team (add-member-to-team klubb team) team))
 
-(defn first-two-in-team [data klubb team]
-                   (filter (fn [x] (= team (get (second x) :team))) (get (get data :no-sequences) klubb)))
+(defn first-two-in-team [klubb team]
+  (filter (fn [x] (= team (get (second x) :team))) klubb))
 
-(defn missing-gender-in-team [data klubb team]
-  (if (= (get (second (first (first-two-in-team data klubb team))) :kön) (get (second (second (first-two-in-team data klubb team))) :kön))
-    (if (= "H" (get (second (first (first-two-in-team data klubb team))) :kön))
+(defn missing-gender-in-team [klubb team]
+  (if (= (get (second (first (first-two-in-team klubb team))) :kön) (get (second (second (first-two-in-team klubb team))) :kön))
+    (if (= "H" (get (second (first (first-two-in-team klubb team))) :kön))
       "D"
       "H")
     :inget))
 
 (defn add-last-member
-  ([data klubb team]
-   (add-member-to-team data klubb team))
-  ([data klubb team kön]
-   (add-member-to-team data klubb team kön))
+  ([klubb team]
+   (add-member-to-team klubb team))
+  ([klubb team kön]
+   (add-member-to-team klubb team kön))
   )
 
-(defn add-third-member-to-team [data klubb team]
-  (if (= :inget (missing-gender-in-team data klubb team))
-    (add-last-member data klubb team)
-    (if (= "H" (missing-gender-in-team data klubb team))
-      (add-last-member data klubb team "H")
-      (add-last-member data klubb team "D"))))
+(defn add-third-member-to-team [klubb team]
+  (if (= :inget (missing-gender-in-team klubb team))
+    (add-last-member klubb team)
+    (if (= "H" (missing-gender-in-team klubb team))
+      (add-last-member klubb team "H")
+      (add-last-member klubb team "D"))))
+
+(defn not-contains? [data key]
+  (not (contains? data key)))
+
+(defn create-team [klubb team]
+  (add-third-member-to-team (add-first-two-members-to-team klubb team)  team))
 
 
-(def data
-  (remove-sequences
-    (key-value-klubb {}
-                     (sort-partions-by-time
-                       (group-by-klubb
-                         (sort-åkare-by-klubb
-                           (key-value-åkare {}
-                                            (to-map
-                                              (split-values
-                                                (split-keys
-                                                  (split-lines
-                                                    (read-file "/home/roland/git/Kodnot-2/resultat.csv"))))))))))))
+(defn add-teams-to-klubb [klubb team]
+  (if (empty? (filter (fn [akare] (not-contains? (second akare) :team))  klubb))
+    klubb
+    (add-teams-to-klubb (create-team klubb team) (inc team))))
 
-(def data2 (add-third-member-to-team (add-first-two-members-to-team data "Klubb 6" "A")  "Klubb 6" "A"))
+(defn add-teams-to-all-klubb [data]
+  (map (fn [x] (add-teams-to-klubb (second x) 0)) (data :no-sequences)))
+
+(defn flatten-akare-teams [data]
+  (filter map? (flatten (add-teams-to-all-klubb data))))
+
+(defn is-three [data]
+  (= 3 (count (second data))))
+
+(defn remove-non-teams [data]
+  (into {} (filter (fn [x] (is-three x)) (sort (group-by (juxt :klubb :team) (flatten-akare-teams data))))))
+
+(defn to-date [date-string]
+  (f/parse (f/formatter "HH:mm:ss.SSS") date-string))
+
+(defn to-long [date]
+  (c/to-long date))
+
+(defn sum-team-times [team]
+  (reduce
+    (fn [x y]
+      (+ x
+         (to-long
+           (to-date (:totaltid  y)))))
+    0
+    team))
+
+(defn get-team-times [data]
+  (sort-by (fn [x] (nth x 2)) (map
+    (fn [x]
+      (update-in x [2]  :num (sum-team-times
+                                           (second
+                                             x))))
+    data)))
+
+(defn klubb-namn [klubb]
+  (first (first klubb)))
+
+(defn åkare-namn [klubb index]
+  (str (:fornamn (nth (second klubb) index)) " " (:efternamn (nth (second klubb) index))))
+
+(defn format-time [time-in-millis]
+  (f/unparse (f/formatter "HH:mm:ss.SSS") (c/from-long time-in-millis)))
+
+(defn print-result [data]
+  (map (fn [x]
+         (println
+           (klubb-namn x)
+           (åkare-namn x 0)
+           (åkare-namn x 1)
+           (åkare-namn x 2)
+           (format-time (nth x 2))
+           )) data))
+
+(defn -main []
+       (print-result
+         (get-team-times
+           (remove-non-teams
+             (remove-sequences
+               (key-value-klubb {}
+                                (sort-partions-by-time
+                                  (group-by-klubb
+                                    (sort-åkare-by-klubb
+                                      (key-value-åkare {}
+                                                       (to-map
+                                                         (split-values
+                                                           (split-keys
+                                                             (split-lines
+                                                               (read-file "/home/roland/git/Kodnot-2/resultat.csv")))))))))))))))
+
